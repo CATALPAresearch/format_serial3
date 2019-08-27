9d308214edfe63013789c56de7ffa334dc8a5776
nise <niels.seidel@nise81.com>
2019-08-27 14:13:32 +0200

    added moment.js for handling date and time

--- amd/src/ladtopics.js
+++ amd/src/ladtopics.js
@@ -1,5 +1,5 @@
 /**
- * Javascript controller for Vue.js and Vi-Two
+ * Main method of the plugin. Load depending javascript and css before starting the timeline dashboard.
  *
  * @module     format/ladtopics
  * @package    format_ladtopics
@@ -9,30 +9,35 @@
  * @since      3.1
  */
 define([
-    'jquery', 'jqueryui', '/moodle/course/format/ladtopics/amd/src/timeline.js', '/moodle/course/format/ladtopics/amd/src/Utils.js'
+    'jquery', 'jqueryui', M.cfg.wwwroot + '/course/format/ladtopics/amd/src/timeline.js', M.cfg.wwwroot + '/course/format/ladtopics/amd/src/Utils.js'
 ],
     function ($, jqueryui, Timeline, Utils) {
-        
+
         require.config({
             enforceDefine: false,
             baseUrl: M.cfg.wwwroot + "/course/format/ladtopics/lib/",
-            paths: { 
+            paths: {
                 "vue259": ["https://cdn.jsdelivr.net/npm/vue@2.5.9/dist/vue", "vue"],
-               // "vuetreeselect": [ "vue-treeselect.min" ],
+                // "vuetreeselect": [ "vue-treeselect.min" ],
                 "crossfilter": ["crossfilter"],
-                "d3": [ "d3.v4.min"],
-                "dc": [ "dc.v3"],
+                "d3": ["d3.v4.min"],
+                "dc": ["dc.v3"],
                 "reductio": ["https://rawgit.com/crossfilter/reductio/master/reductio", "reductio"],
                 "universe": ["https://npmcdn.com/universe@latest/universe", "universe"],
-                "bootstrap_select": ["https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/js/bootstrap-select.min", "bootstrap-select.min"]
+                "bootstrap_select": ["https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/js/bootstrap-select.min", "bootstrap-select.min"],
+                "moment224": ["moment.min"]
+
                 /*
                 "crossfilter2": "crossfilter.v2",
                 "crossfilter": "https://cdnjs.cloudflare.com/ajax/libs/crossfilter/1.3.5/crossfilter",
                 */
             },
             shim: {
-                'vue259' : {
+                'vue259': {
                     exports: 'Vue'
+                },
+                "moment224":{
+                    exports: 'moment'
                 },
                 /*'vuetreeselect': {
                     deps: ['vue259'],
@@ -53,50 +58,49 @@ define([
             }
         });
 
-        
+
         function start() {
-        
+
             // add style sheets        
             const css = [
-                //"/moodle/course/format/ladtopics/css/bootstrap3.min.css", 
                 M.cfg.wwwroot + "/course/format/ladtopics/css/ladtopics.css",
                 M.cfg.wwwroot + "/course/format/ladtopics/css/dc.css",
                 M.cfg.wwwroot + "/course/format/ladtopics/css/bootstrap-select.min.css"
             ];
             let link = '';
-            for(let i=0; i< css.length;i++){
+            for (let i = 0; i < css.length; i++) {
                 link = document.createElement("link");
                 link.rel = "stylesheet";
                 link.type = "text/css";
                 link.href = css[i];
                 document.getElementsByTagName("head")[0].appendChild(link);
             }
-                    
+
             //$('#accordion').tab();
 
             require([
-               'vue259',
-               // 'vuetreeselect', 
-                'crossfilter', 
-                'd3', 
-                'dc', 
-                'reductio', 
+                'vue259',
+                'crossfilter',
+                'd3',
+                'dc',
+                'reductio',
                 'universe',
-                'bootstrap_select'
-            ], function (vue, crossfilter, d3, dc, reduction, universe,bselect) {
+                'bootstrap_select',
+                'moment224'
+            ], function (vue, crossfilter, d3, dc, reduction, universe, bselect, moment) {
                 const utils = new Utils(dc, d3);
-                new Timeline(vue, d3, dc, crossfilter, bselect, utils);
+                new Timeline(vue, d3, dc, crossfilter, moment, bselect, utils);
             });
         }
 
         return {
             init: function () {
-                try{
+                try {
                     start();
-                }catch(e){
+                } catch (e) {
                     console.error(e);
                 }
-                
+
             }
         };
     }); 
