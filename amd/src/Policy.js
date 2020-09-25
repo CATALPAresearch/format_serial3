277a66b2ec6e7570a2fd1bf49dee41b75408f5da
Niels Seidel <niels.seidel@fernuni-hagen.de>
2020-09-25 11:19:19 +0200

    improved layout

--- amd/src/Policy.js
+++ amd/src/Policy.js
@@ -24,7 +24,7 @@ define([
         require.config({
             enforceDefine: false,
             paths: {
-                "moment226": [M.cfg.wwwroot + "/course/format/ladtopics/lib/build/moment-with-locales.min"],
+                "moment226": [M.cfg.wwwroot + "/course/format/ladtopics/lib/build/moment-with-locales.min.js"],
             },
             shim: {
                 "moment226": {
@@ -63,22 +63,29 @@ define([
                     template: `
                             <div id="policy-container">
                                 <h3 class="my-4">Zustimmung und Wideruf zur Datenverarbeitung</h3>
-                                <div class="mb-3" v-for="p in policies">
+                                <div class="row mb-3 border-bottom pb-2" v-for="p in policies">
                                     <!-- 
                                     href="http://127.0.0.1/moodle/admin/tool/policy/view.php?policyid=1&versionid=9&returnurl=http%3A%2F%2F127.0.0.1%2Fmoodle%2Fadmin%2Ftool%2Fpolicy%2Fuser.php%3Fuserid%3D2"
                                     -->
-                                    <a target="s" class="bold">{{p.name}}</a> in der Version vom 
-                                    <span class="">{{ convertTime(p.creation) }}</span>
+                                    <div class="col-10">
+                                        <a target="s" class="bold">{{p.name}}</a> in der Version vom 
+                                        <span class="">{{ convertTime(p.creation) }}</span>
+                                        <span v-if="p.status==1">
+                                            haben Sie 
+                                            am <span class="">{{ convertTime(p.acceptance) }}</span>
+                                            zugestimmt <i class="fa fa-check ml-3"></i>
+                                        <span v-if="p.status==0">
+                                            <i class="fa fa-times ml-3"></i>
+                                        </span>
+                                    </div>
+                                    <div class="col-2">
+                                        <span v-if="p.status==1">
+                                            <a :href="getLink(p,0)" class="right btn btn-sm btn-outline-primary">Zustimmung widerufen</a></span>
+                                        <span v-if="p.status==0">
+                                            <a :href="getLink(p,1)" class="btn btn-sm btn-primary">Akzeptieren</a>
+                                        </span>
+                                    </div>
                                     
-                                    <span v-if="p.status==1">
-                                        haben Sie 
-                                         am <span class="">{{ convertTime(p.acceptance) }}</span>
-                                         zugestimmt <i class="fa fa-check ml-3"></i>
-                                        <a :href="getLink(p,0)" class="right btn btn-sm btn-outline-primary">Zustimmung widerufen</a></span>
-                                    <span v-if="p.status==0">
-                                        <i class="fa fa-times ml-3"></i>
-                                        <a :href="getLink(p,1)" class="btn btn-sm btn-primary">Akzeptieren</a>
-                                    </span>
                                 </div>
                             </div>
                         `
