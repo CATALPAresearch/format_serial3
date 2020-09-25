2a6c6dcfd9c2c434da407b9fa53db5f62667d34b
Niels Seidel <niels.seidel@fernuni-hagen.de>
2020-09-25 08:57:09 +0200

    Added policy overview

---
+++ amd/src/Policy.js
@@ -0,0 +1,89 @@
+/* eslint-disable spaced-comment */
+/* eslint-disable require-jsdoc */
+/**
+ * Main method of the plugin. Load depending javascript and css before starting the timeline dashboard.
+ *
+ * @module     format/ladtopics
+ * @class      LADTopics
+ * @copyright  2019 Niels Seidel <niels.seidel@fernuni-hagen.de>
+ * @license    MIT
+ * @since      3.1
+ * 
+ * TODO
+ * - set link to policy text
+ * - submit accept/withdraw
+ */
+define([
+    'jquery',
+    M.cfg.wwwroot + "/course/format/ladtopics/lib/build/vue.min.js",
+    M.cfg.wwwroot + "/course/format/ladtopics/lib/build/moment-with-locales.min"
+
+],
+    function ($, Vue, moment) {
+
+        require.config({
+            enforceDefine: false,
+            paths: {
+                "moment226": [M.cfg.wwwroot + "/course/format/ladtopics/lib/build/moment-with-locales.min"],
+            },
+            shim: {
+                "moment226": {
+                    exports: 'moment'
+                }
+            }
+        });
+
+        return {
+            init: function (policies) {
+                return new Vue({
+                    el: 'policy-container',
+
+                    data: function () {
+                        return {
+                            policies: policies
+                        }
+                    },
+
+                    mounted: function () {
+                        
+                    },
+
+                    methods:{
+                        accept: function(){
+
+                        },
+                        convertTime: function(utc){
+                            return moment.unix(utc).format("DD.MM.YYYY");
+                        },
+                        getLink: function(p, action){
+                            return M.cfg.wwwroot + '/course/format/ladtopics/policy.php?policy='+p.id+'&version='+p.version+'&action='+action;
+                        }
+                    },
+
+                    template: `
+                            <div id="policy-container">
+                                <h3 class="my-4">Zustimmung und Wideruf zur Datenverarbeitung</h3>
+                                <div class="mb-3" v-for="p in policies">
+                                    <!-- 
+                                    href="http://127.0.0.1/moodle/admin/tool/policy/view.php?policyid=1&versionid=9&returnurl=http%3A%2F%2F127.0.0.1%2Fmoodle%2Fadmin%2Ftool%2Fpolicy%2Fuser.php%3Fuserid%3D2"
+                                    -->
+                                    <a target="s" class="bold">{{p.name}}</a> in der Version vom 
+                                    <span class="">{{ convertTime(p.creation) }}</span>
+                                    
+                                    <span v-if="p.status==1">
+                                        haben Sie 
+                                         am <span class="">{{ convertTime(p.acceptance) }}</span>
+                                         zugestimmt <i class="fa fa-check ml-3"></i>
+                                        <a :href="getLink(p,0)" class="right btn btn-sm btn-outline-primary">Zustimmung widerufen</a></span>
+                                    <span v-if="p.status==0">
+                                        <i class="fa fa-times ml-3"></i>
+                                        <a :href="getLink(p,1)" class="btn btn-sm btn-primary">Akzeptieren</a>
+                                    </span>
+                                </div>
+                            </div>
+                        `
+
+                });
+            }
+        };
+    });
---
+++ policy.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once(dirname(__FILE__) . '/../../../config.php');
+
+$context = context_system::instance();
+global $USER, $PAGE, $DB, $CFG;
+$PAGE->set_context($context);
+$PAGE->set_url($CFG->wwwroot.'/course/format/ladtopics/policy.php');
+$PAGE->set_pagelayout('course');
+$PAGE->set_title("Zustimmung und Richtlinien");
+echo $OUTPUT->header();
+
+
+global $DB, $USER;
+
+// change policy status
+if(isset($_GET['policy']) && isset($_GET['action']) && isset($_GET['version'])){
+    //$query = '';
+    //$res = $DB->set_records_sql($query, array((int)$USER->id));
+}
+
+// fetch policies
+$query = '
+SELECT v.name, a.status, a.timecreated as acceptance, v.timecreated as creation, p.id as id, v.id as version
+From '.$CFG->prefix.'tool_policy as p
+JOIN '.$CFG->prefix.'tool_policy_acceptances as a ON p.currentversionid = a.policyversionid
+JOIN '.$CFG->prefix.'tool_policy_versions as v ON v.id = p.currentversionid
+WHERE a.userid = ?
+    '; 
+$res = $DB->get_records_sql($query, array((int)$USER->id));
+//get_records("tool_policy_acceptances", array("userid" => (int)$USER->id ));
+echo '<policy-container></policy-container>';
+$PAGE->requires->js_call_amd('format_ladtopics/Policy', 'init', array('policies'=>$res));
+echo $OUTPUT->footer();
+
