1151813dad8c2c2c586690508e99f8d35383edda
Marc Burchart <marc.burchart@fernuni-hagen.de>
2020-10-14 22:49:39 +0000

    created a first imp. of the user page

---
+++ amd/src/ladAnalytics.js
@@ -0,0 +1,197 @@
+/**
+* @author Marc Burchart
+* @email marc.burchart@fernuni-hagen.de
+* @description  
+* @version 1.0.0
+*/
+
+define([
+    'jquery',
+    M.cfg.wwwroot + "/course/format/ladtopics/lib/build/vue.min.js",
+    M.cfg.wwwroot + "/course/format/ladtopics/lib/build/moment-with-locales.min.js"
+],
+    function($, Vue, moment){
+        require.config({
+            enforceDefine: false,
+            paths: {
+                "moment226": [M.cfg.wwwroot + "/course/format/ladtopics/lib/build/moment-with-locales.min.js"],
+            },
+            shim: {
+                "moment226": {
+                    exports: 'moment'
+                }
+            }
+        });
+
+        $('#page-header').hide();
+        $('#page-content').css('margin-top', "57px");
+
+        return {
+            init: function(users){
+                return new Vue(
+                    {
+                        el: 'analytics-dashboard',
+                        data: {
+                            users: users,
+                            currentPage: 'user',
+                            currentUser: null
+                        },
+                        computed:{
+                            showHome: function(){
+                                return this.currentPage === 'home';
+                            },
+                            showUser: function()
+                            {
+                                return this.currentPage === 'user';
+                            }
+                        },
+                        methods: {
+                            setCurrentPage: function(page){
+                                this.currentPage = page;
+                            },
+                            setUser: function(user){
+                                this.currentUser = user;
+                            },
+                            convertUnix: function(unix){
+                                let date = new Date(unix * 1000);
+                                let day = date.getDate();
+                                let month = date.getMonth();
+                                let year = date.getFullYear();
+                                let hours = date.getHours();
+                                let minutes = date.getMinutes();
+                                return `${day.toString().length !== 1 ? day : "0" + day}.${month.toString().length !== 1 ? month : "0" + month}.${year} ${hours.toString().length !== 1 ? hours : "0" + hours}:${minutes.toString().length !== 1 ? minutes : "0" + minutes}`;                         
+                            },
+                            convertMoment: function(date){
+                                return moment(date).format('DD.MM.YYYY HH:mm');
+                            },
+                            cleanTitle: function(title){
+                                title = (title+'').toLowerCase();
+                                title = (title + '').replace(/^([a-z])|\s+([a-z])/g, function ($1) {
+                                    return $1.toUpperCase();
+                                });
+                                return title;
+                            },
+                            getMSStatus: function(status){
+                                switch(status){
+                                    case 'urgent': return 'dringlich';
+                                    case 'ready': return 'bereit';
+                                    case 'missed': return 'abgelaufen';
+                                    case 'reflected': return 'reflektiert';
+                                }
+                            }
+                        },
+                        template: `
+                        <div>
+                            <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+                                <a class="navbar-brand" href="#"><span class="text-white">LADTopics Analytics Dashboard</span></a>
+                                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                                    <span class="navbar-toggler-icon"></span>
+                                </button>
+                                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                                    <div class="navbar-nav">
+                                        <a class="nav-item nav-link active" v-on:click="setCurrentPage('home')" href="#"><span class="text-white">Meilensteine</span></a>
+                                        <a class="nav-item nav-link" v-on:click="setCurrentPage('user')" href="#"><span class="text-white">Benutzer</span></a>
+                                        <a class="nav-item nav-link" href="#"><span class="text-white">xxx</span></a>
+                                    </div>
+                                </div>                  
+                            </nav>
+                            <!-- Home Dashboard -->
+                            <div class="py-2 px-1" v-if="showHome">
+                                home
+                            </div>
+                            <!-- User Dashboard -->
+                            <div class="py-2" v-if="showUser">
+                                <div class="container-fluid">
+                                    <div class="row">
+                                        <div class="col-8 bg-secondary">
+                                            <div v-if="currentUser !== null" class="px-4 py-3" >
+                                                <h2 style="margin-bottom: 0px;"><b>{{currentUser.firstname+" "+currentUser.lastname}}</b></h2>
+                                                <span><<i>{{currentUser.email}}</i>></span>
+                                                <table class="table table-responsive">
+                                                    <tbody>
+                                                        <tr>                                                            
+                                                            <td style="border-top-style: none;"><i class="fa fa-calendar-alt"></i> Erster Login {{ convertUnix(currentUser.firstaccess) }}</td>   
+                                                            <td style="border-top-style: none;"><i class="fa fa-clock"></i> Letzter Login {{ convertUnix(currentUser.lastaccess) }}</td>
+                                                            <td style="border-top-style: none;">
+                                                                <i class="fa fa-check text-danger" v-if="currentUser.suspended == true"></i>
+                                                                <i class="fa fa-times text-success" v-if="currentUser.suspended == false"></i>
+                                                                Suspendiert
+                                                            </td>
+                                                            <td style="border-top-style: none;">
+                                                                <i class="fa fa-check text-danger" v-if="currentUser.deleted == true"></i>
+                                                                <i class="fa fa-times text-success" v-if="currentUser.deleted == false"></i>
+                                                                Gelöscht
+                                                            </td>
+                                                        </tr>                                                       
+                                                    </tbody>
+                                                </table>
+                                                <h4>Aktuelle Meilensteine</h4>
+                                                <div id="mstones" v-if="currentUser.milestones !== null">
+                                                    <div class="card" v-for="milestone in currentUser.milestones.elements" v-bind:key="milestone.id">
+                                                        <div class="card-header" v-bind:id="'heading-ms'+milestone.id">
+                                                            <h5 class="mb-0">
+                                                                <button class="btn btn-link collapsed" data-toggle="collapse" v-bind:data-target="'#collapse-ms'+milestone.id" aria-expanded="false" v-bind:aria-controls="'collapse-ms'+milestone.id">
+                                                                    {{ milestone.name }}
+                                                                </button>
+                                                            </h5>
+                                                        </div>
+                                                        <div v-bind:id="'collapse-ms'+milestone.id" class="collapse" v-bind:aria-labelledby="'collapse-ms'+milestone.id" data-parent="#mstones">
+                                                            <div class="card-body">
+                                                                <table class="table table-responsive">
+                                                                    <tbody>                                                                        
+                                                                        <tr>
+                                                                            <td>Lernziel</td>
+                                                                            <td>{{ milestone.objective }}</td>
+                                                                        </tr>
+                                                                        <tr>
+                                                                            <td>Status</td>
+                                                                            <td>{{ getMSStatus(milestone.status) }}</td>
+                                                                        </tr>
+                                                                        <tr>
+                                                                            <td>Beginn</td>
+                                                                            <td>{{ convertMoment(milestone.start) }}</td>
+                                                                        </tr>
+                                                                        <tr>
+                                                                            <td>Termin</td>
+                                                                            <td>{{ convertMoment(milestone.start) }}</td>
+                                                                        </tr>
+                                                                        <tr>
+                                                                            <td>Ressourcen</td>
+                                                                            <td>
+                                                                                <ul style="list-style-type:none; padding-left: 0px;">
+                                                                                    <li v-for="ressource in milestone.resources">
+                                                                                        <i class="fa fa-check text-success" v-if="ressource.checked === true"></i>
+                                                                                        <i class="fa fa-times text-danger" v-if="ressource.checked !== true"></i>
+                                                                                        {{ ressource.instance_title }}
+                                                                                        [<i>{{ cleanTitle(ressource.instance_type) }}</i>] 
+                                                                                    </li>
+                                                                                </ul>
+                                                                            </td>
+                                                                        </tr>                                                                        
+                                                                    </tbody>
+                                                                </table>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-4 bg-secondary">
+                                            <div class="form-group py-2">
+                                                <!-- <label for="chooseUser">Benutzer*in</label> -->
+                                                <select multiple class="form-control" id="chooseUser">
+                                                    <option v-for="user in users" v-bind:key="user.id" v-on:click="setUser(user)" >{{user.firstname+" "+user.lastname+" ("+user.username+")"}}</option>
+                                                </select>
+                                            </div>
+                                        </div>                                       
+                                    </div>
+                                </div>
+                            </div>  
+                        </div>
+                        `
+                    }
+                );
+            }
+        }
+    }
+);
---
+++ analytics/ladtopics.php
@@ -0,0 +1,105 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * 
+ * @author Marc Burchart <marc.burchart@fernuni-hagen.de>
+ * 
+ */
+
+require_once(dirname(__FILE__) . '/../../../../config.php');
+$context = context_system::instance();
+$PAGE->set_context($context);
+$PAGE->set_url($CFG->wwwroot.'/course/format/ladtopics/analytics/ladtopics.php');
+require_login();
+
+$PAGE->set_pagelayout('course');
+$PAGE->set_title(get_string('surveyTitle', 'format_ladtopics'));
+$PAGE->set_heading(get_string('surveyHeadline', 'format_ladtopics'));
+
+if(!isset($_GET['c']) || $DB->count_records('course', array('id' => $_GET['c'])) !== 1){
+    // Kurs nicht gefunden
+    redirect(new moodle_url("/"), 'Es wurde kein passender Kurs übergeben!', null, \core\output\notification::NOTIFY_WARNING);
+} else {
+    $courseid = $_GET['c'];
+    // Kurs gefunden
+    $permission = new format_ladtopics\permission\course((int)$USER->id, $courseid);
+    if(!$permission->isAnyKindOfModerator()){
+        // Keine Berechtigung
+        redirect(new moodle_url("/"), 'Sie haben keine Berechtigung das Dashboard einzusehen!', null, \core\output\notification::NOTIFY_WARNING);
+    } else {
+        // Zugriff gewährt
+        echo $OUTPUT->header();
+        echo '<analytics-dashboard></analytics-dashboard>';
+        // Alle User erhalten
+        $context = $permission->getCourseContext();
+        $enrollments = get_enrolled_users($context);       
+        $users = array();
+        foreach($enrollments as $user){
+            $u = new stdClass();
+            $u->id = $user->id;
+            $u->firstname = ucfirst(strtolower($user->firstname));
+            $u->lastname = ucfirst(strtolower($user->lastname));
+            $u->username = $user->username;
+            $u->email = strtolower($user->email);
+            $u->lang = $user->lang;
+            $u->deleted = $user->deleted;
+            $u->suspended = $user->suspended;
+            $u->firstaccess = $user->firstaccess;
+            $u->lastaccess = $user->lastaccess;
+            // Milestones
+            $sql = '
+                SELECT t.milestones, t.settings, t.timemodified 
+                FROM '.$CFG->prefix.'ladtopics_milestones AS t
+                WHERE   
+                    t.course = ' . (int)$courseid . ' 
+                    AND t.userid = ' . (int)$user->id . '
+                ORDER BY t.timemodified DESC
+                LIMIT 1';
+            $ms = $DB->get_record_sql($sql);
+            if(is_object($ms)){
+                $u->milestones = new stdClass();
+                $u->milestones->modified = $ms->timemodified;
+                $u->milestones->elements = json_decode($ms->milestones);
+                $u->milestones->count = count($u->milestones->elements);
+            } else {
+                $u->milestones = null;
+            }
+            // Preferences
+            $surveyDone = $DB->get_record("user_preferences", array(
+                'name' => 'ladtopics_survey_done-course-' . (int)$courseid,
+                'userid'=>(int)$user->id
+            ));
+            $surveyData = $DB->get_record("user_preferences", array(
+                'name' => 'ladtopics_survey_results-course-' . (int)$courseid,
+                'userid'=>(int)$user->id
+            ));
+            if($surveyDone !== false && is_object($surveyData) && isset($surveyData->value)){
+                $u->initialSurvey = json_decode($surveyData->value);
+            } else {
+                $u->initialSurvey = null;
+            }           
+            $users[] = $u;
+        }         
+        // The Array which should be send to vue.
+        $send = array(
+            'users' => $users
+        );
+        $PAGE->requires->js_call_amd('format_ladtopics/ladAnalytics', 'init', $send);
+        echo $OUTPUT->footer();
+    }
+}
