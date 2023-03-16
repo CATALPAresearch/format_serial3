4b668725619cd619a7a7ce86c77df73a81621938
valerie177 <valeriemeyer@gmx.net>
2023-03-16 09:30:26 +0100

    Add popover content

---
+++ vue/components/PopoverContent.vue
@@ -0,0 +1,105 @@
+<template>
+    <div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="completed" v-model="completed" @change="toggleCompletion">
+            <label class="form-check-label" for="completed">Abgeschlossen</label>
+        </div>
+
+        <p class="mb-1 mt-2">Rate your understanding of the material:</p>
+        <div class="row ml-1">
+            <div class="form-check mb-2 col-5 pr-0">
+                <input class="form-check-input" type="radio" name="userUnderstanding" id="weak" value="1" v-model="rating" />
+                <label class="form-check-label" for="weak">Schlecht</label>
+            </div>
+            <div class="form-check mb-2 col-3 pr-0">
+                <input class="form-check-input" type="radio" name="userUnderstanding" id="ok" value="2" v-model="rating" />
+                <label class="form-check-label" for="ok">Ok</label>
+            </div>
+            <div class="form-check mb-2 col-4 pr-0">
+                <input class="form-check-input" type="radio" name="userUnderstanding" id="strong" value="3" v-model="rating" />
+                <label class="form-check-label" for="strong">Gut</label>
+            </div>
+        </div>
+
+        <div class="py-1">
+            <a href="#">
+                Ask for help
+                <i class="fa fa-arrow-right" aria-hidden="true"></i>
+            </a>
+        </div>
+        <div class="py-1">
+            <a :href="activity.url">
+                Gehe zu {{ activity.name }}
+                <i class="fa fa-arrow-right" aria-hidden="true"></i>
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+import {ajax} from '../store';
+
+
+export default {
+    name: "PopoverContent",
+
+    props: {
+        activity: { type: Object, required: true},
+    },
+
+    data() {
+        return {
+            rating: null,
+            completed: false,
+        };
+    },
+
+    watch: {
+        rating(newVal) {
+            this.updateRating(newVal);
+        },
+    },
+
+    async created() {
+        const response = await ajax('format_ladtopics_getUserUnderstanding', {
+            course: 4,
+            activityid: this.activity.id,
+        })
+
+        if (response.success) {
+            const res = JSON.parse(response.data)
+
+            if (res.rating) {
+                this.rating = Number(res.rating)
+            }
+            if (res.completed) {
+                this.completed = Number(res.completed)
+            }
+        }
+    },
+
+    methods: {
+        async updateRating (newVal) {
+            await ajax("format_ladtopics_setUserUnderstanding", {
+                course: 4,
+                activityid: this.activity.id,
+                rating: newVal,
+            })
+            this.$emit('rating-updated', newVal)
+        },
+
+        async toggleCompletion () {
+            await ajax('format_ladtopics_setCompletionStatus', {
+                course: 4,
+                activityid: this.activity.id,
+                completed: this.completed,
+            })
+            this.$emit('completion-updated', this.completed)
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
