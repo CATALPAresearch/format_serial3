352462518ecd667027eefe4d0bfb30aaec0c7afe
nise <niels.seidel@nise81.com>
2024-05-29 00:17:56 +0200

    init branch

--- version.php
+++ version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023092965;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version = 2024052614;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires = 2017050500;        // Requires this Moodle version.
 $plugin->component = 'format_serial3';    // Full name of the plugin (used for diagnostics).
